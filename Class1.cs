a1c5e8119079843c9dad6471b60d11a6841364bd
AdrianCzerwonka1 <116648427+AdrianCzerwonka1@users.noreply.github.com>
2024-05-24 14:20:49 -0400

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Person
+    {
+        private string FName;
+        private string MName;
+        private string LName;
+        private string Street1;
+        private string Street2;
+        private string City;
+        private string State;
+        private string Zip;
+        private string Phone;
+        private string Email;
+
+        public string FirstName
+        {
+            get
+            {
+                return FName;
+            }
+            set
+            {
+                FName = value;
+            }
+        }
+        public string MidName
+        {
+            get
+            {
+                return MName;
+            }
+            set
+            {
+                MName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return LName;
+            }
+            set
+            {
+                LName = value;
+            }
+        }
+        public string StreetOne
+        {
+            get
+            {
+                return Street1;
+            }
+            set
+            {
+                Street1 = value;
+            }
+        }
+        public string StreetTwo
+        {
+            get
+            {
+                return Street2;
+            }
+            set
+            {
+                Street2 = value;
+            }
+        }
+        public string PubCity
+        {
+            get
+            {
+                return City;
+            }
+            set
+            {
+                City = value;
+            }
+        }
+        public string PubState
+        {
+            get
+            {
+                return State;
+            }
+            set
+            {
+                State = value;
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return Zip;
+            }
+            set
+            {
+                Zip = value;
+            }
+        }
+        public string PhoneNum
+        {
+            get
+            {
+                return Phone;
+            }
+            set
+            {
+                Phone = value;
+            }
+        }
+        public string UserEmail
+        {
+            get
+            {
+                return Email;
+            }
+            set
+            {
+                Email = value;
+            }
+        }
+
+    }
+
+
+
+
+    public class Insta : Person
+    {
+        private string Instagram;
+        private string CellPhone;
+        public string UserInsta
+        {
+            get
+            {
+                return Instagram;
+            }
+            set
+            {
+                Instagram = value;
+            }
+        }
+
+            public string Cell
+        {
+            get
+            {
+                return CellPhone;
+            }
+            set
+            {
+                CellPhone = value;
+            }
+
+        } } }
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Insta employee = new Insta();
+
+            employee.FirstName = textBox1.Text;
+            employee.MidName = textBox2.Text;
+            employee.LastName = textBox3.Text;
+            employee.StreetOne = textBox4.Text;
+            employee.StreetTwo = textBox5.Text;
+            employee.PubCity = textBox6.Text;
+            employee.PubState = textBox7.Text;
+            employee.ZipCode = textBox8.Text;
+            employee.PhoneNum = textBox9.Text;
+            employee.UserEmail = textBox10.Text;
+            employee.UserInsta = textBox11.Text;
+            employee.Cell = textBox12.Text;
+
+            label11.Text = "Employee Name " + employee.FirstName + " " + employee.MidName + " " + employee.LastName + " . The Employee Address is " + employee.StreetOne + " " + employee.StreetTwo + " " + employee.PubCity + "  " + employee.PubState + " " + employee.ZipCode + " . Personal Employee info. " + employee.PhoneNum + " " + employee.UserEmail + " Users Instagram " + employee.UserInsta + " Secondary Cell " + employee.Cell;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
